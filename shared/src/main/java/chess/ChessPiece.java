635b4431b1b03f9fb0a0d9a00a8671fc150780e0
Faith Kirkham <faithkirkham12@gmail.com>
2025-09-24 22:41:50 -0600

    added the phase 1 test files, removed redundant lines

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -15,12 +15,10 @@ public class ChessPiece {
 
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
-    //private boolean hasMoved;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
-        //this.hasMoved = false;
     }
 
     /**
