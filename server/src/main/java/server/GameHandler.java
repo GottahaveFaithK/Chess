bb1322cc3035d18263f081cf8181b5efa50859b1
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-17 17:31:13 -0600

    Game Handler outline (phase 3)

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -1,4 +1,25 @@
 package server;
 
+import service.GameService;
+import io.javalin.http.Context;
+
+
 public class GameHandler {
+    private final GameService gameService;
+
+    public GameHandler(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    public void createGame(Context ctx) {
+        //TODO impl this
+    }
+
+    public void listGames(Context ctx) {
+        //TODO impl file
+    }
+
+    public void joinGame(Context ctx) {
+        //TODO impl file
+    }
 }
