7af8daba328c924aee5f525de91a44380556d380
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-17 17:33:43 -0600

    restructured server to work with gamehandler  (phase 3)

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,12 +2,16 @@ package server;
 
 import dataaccess.*;
 import io.javalin.*;
+import service.ClearService;
+import service.GameService;
 import service.UserService;
 
 public class Server {
 
     private final Javalin server;
     private final UserHandler userHandler;
+    private final GameHandler gameHandler;
+    //private final ClearHandler clearHandler;
     private UserDAO userDAO;
 
     public Server() {
@@ -17,10 +21,12 @@ public class Server {
         AuthDAO authDAO = new MemoryAuthDAO();
 
         UserService userService = new UserService(userDAO, authDAO);
-        //GameService gameService = new GameService(gameDAO, authDAO);
-        //ClearService clearService = new ClearService(userDAO, gameDAO, authDAO);
+        GameService gameService = new GameService(gameDAO, authDAO);
+        ClearService clearService = new ClearService(gameDAO, userDAO, authDAO);
 
         userHandler = new UserHandler(userService);
+        gameHandler = new GameHandler(gameService);
+        //clearHandler = new ClearHandler(clearService);
 
         server = Javalin.create(config -> config.staticFiles.add("web"));
 
@@ -33,9 +39,9 @@ public class Server {
         server.post("user", userHandler::register);
         server.post("session", userHandler::login);
         server.delete("session", userHandler::logout);
-        //server.get("game", this::listGames);
-        //server.post("game", this::createGame);
-        //server.put("game", this::joinGame);
+        server.get("game", gameHandler::listGames);
+        server.post("game", gameHandler::createGame);
+        server.put("game", gameHandler::joinGame);
     }
 
     public int run(int desiredPort) {
