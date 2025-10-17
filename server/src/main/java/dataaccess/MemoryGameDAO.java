0a1e93f1e3cd7663fc95ad255497f4a71e0f3c9b
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-17 15:33:26 -0600

    scaffolding for game interface and memoryDAO (phase 3)

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,48 @@
 package dataaccess;
 
-public class MemoryGameDAO {
+
+import chess.ChessGame;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MemoryGameDAO implements GameDAO {
+    private HashMap<String, GameData> gameStorage = new HashMap<>();
+
+    @Override
+    public void createGame(GameData game) {
+        //TODO impl this
+    }
+
+    @Override
+    public void updateGame(GameData game) {
+        //TODO impl this
+    }
+
+    @Override
+    public GameData getGame(int id) {
+        GameData test = new GameData(67, "white", "black", "hades",
+                new ChessGame());
+        return test;
+        //TODO  test interface with this
+    }
+
+    @Override
+    public List<GameData> listGames() {
+        List<GameData> test = new ArrayList<>();
+        return test;
+        //TODO impl this
+    }
+
+    @Override
+    public void deleteGame(GameData game) {
+        //TODO impl this
+    }
+
+    @Override
+    public void deleteAllGames() {
+        //TODO impl this
+    }
 }
