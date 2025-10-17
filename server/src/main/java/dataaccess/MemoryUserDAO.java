f68c95a3a97b6d0d40f3f6fc8d21dab43080b12d
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-17 15:38:16 -0600

    scaffolding for user interface and memoryDAO (phase 3)

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,31 @@
 package dataaccess;
 
-public class MemoryUserDAO {
+import model.UserData;
+
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO {
+    private HashMap<String, UserData> userStorage = new HashMap<>();
+
+    @Override
+    public void createUser(UserData user) {
+        //TODO impl this
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        UserData test = new UserData("cow", "password", "cow@gmail.com");
+        return test;
+        //TODO test this to see if interface is working
+    }
+
+    @Override
+    public void deleteUser(UserData user) {
+        //TODO impl this
+    }
+
+    @Override
+    public void deleteAllUsers() {
+        //TODO impl this
+    }
 }
