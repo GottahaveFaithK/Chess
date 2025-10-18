39248d3956508ac3e7dffecae79c7649e1933050
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-18 11:32:03 -0600

    created a registerNegative test (phase 3)

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,9 +1,6 @@
 package service;
 
-import dataaccess.AuthDAO;
-import dataaccess.MemoryAuthDAO;
-import dataaccess.MemoryUserDAO;
-import dataaccess.UserDAO;
+import dataaccess.*;
 import model.UserData;
 import org.junit.jupiter.api.Test;
 
@@ -12,9 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserServiceTest {
 
     @Test
-    void register() {
+    void registerPositive() {
         var user = new UserData("joe", "j@j", "j");
-        var at = "xyz";
 
         UserDAO userDAO = new MemoryUserDAO();
         AuthDAO authDAO = new MemoryAuthDAO();
@@ -23,6 +19,18 @@ class UserServiceTest {
         assertEquals(res.username(), user.username());
         assertNotNull(res.authToken());
         assertEquals(String.class, res.authToken().getClass());
+    }
+
+    @Test
+    void registerNegative() {
+        var user = new UserData("joe", "j@j", "j");
+
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        UserService userService = new UserService(userDAO, authDAO);
+        userService.register(user);
+        assertThrows(DataAccessException.class, () -> userService.register(user));
+
     }
 
 }
