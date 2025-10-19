46ff8027fd2816dc52b68464a9ac7b5241b84867
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-18 19:15:39 -0600

    register and login now handle bad requests (phase 3)

--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -3,6 +3,7 @@ package server;
 import com.google.gson.Gson;
 import io.javalin.http.Context;
 import model.UserData;
+import service.ResponseException;
 import service.UserService;
 
 public class UserHandler {
@@ -13,24 +14,40 @@ public class UserHandler {
     }
 
     public void register(Context ctx) {
-        var serializer = new Gson();
-        UserData request = serializer.fromJson(ctx.body(), UserData.class);
+        String username = ctx.formParam("username");
+        String password = ctx.formParam("password");
+        String email = ctx.formParam("email");
 
-        var res = userService.register(request);
+        if (username == null || username.isEmpty() ||
+                password == null || password.isEmpty() ||
+                email == null || email.isEmpty()) {
+            throw new ResponseException("Error: bad request", 400);
+        } else {
+            var serializer = new Gson();
+            UserData request = serializer.fromJson(ctx.body(), UserData.class);
 
-        var response = serializer.toJson(res);
-        ctx.result(response);
+            var res = userService.register(request);
+
+            var response = serializer.toJson(res);
+            ctx.result(response);
+        }
     }
 
     public void login(Context ctx) {
-        var serializer = new Gson();
-        UserData request = serializer.fromJson(ctx.body(), UserData.class);
+        String username = ctx.formParam("username");
+        String password = ctx.formParam("password");
 
-        var res = userService.login(request);
+        if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
+            throw new ResponseException("Error: bad request", 400);
+        } else {
+            var serializer = new Gson();
+            UserData request = serializer.fromJson(ctx.body(), UserData.class);
 
-        var response = serializer.toJson(res);
-        ctx.result(response);
+            var res = userService.login(request);
 
+            var response = serializer.toJson(res);
+            ctx.result(response);
+        }
     }
 
     public void logout(Context ctx) {
