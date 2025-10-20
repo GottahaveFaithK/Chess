8097e8b389dbe8ebe6495642b8779550b59e4a66
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-20 16:39:21 -0600

    tested error 400 and 401 for create game (phase 3)

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -23,4 +23,34 @@ public class GameServiceTest {
         var newRes = gameService.createGame("myGame", res.authToken());
         assertEquals(2, newRes);
     }
+
+    @Test
+    void createGameNoName() {
+        var user = new UserData("joe", "j@j", "j@jmail.com");
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+        UserService userService = new UserService(userDAO, authDAO);
+        var res = userService.register(user);
+        GameService gameService = new GameService(gameDAO, authDAO);
+        ResponseException ex = assertThrows(ResponseException.class, ()
+                -> gameService.createGame("", res.authToken()));
+        assertEquals(400, ex.getHttpResponseCode());
+        assertEquals("Error: bad request", ex.getMessage());
+    }
+
+    @Test
+    void createGameNoAuth() {
+        var user = new UserData("joe", "j@j", "j@jmail.com");
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+        UserService userService = new UserService(userDAO, authDAO);
+        userService.register(user);
+        GameService gameService = new GameService(gameDAO, authDAO);
+        ResponseException ex = assertThrows(ResponseException.class, ()
+                -> gameService.createGame("Game", ""));
+        assertEquals(401, ex.getHttpResponseCode());
+        assertEquals("Error: unauthorized", ex.getMessage());
+    }
 }
