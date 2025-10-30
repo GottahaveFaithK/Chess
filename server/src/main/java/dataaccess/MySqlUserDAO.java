43995ef9f819c96cb3eee8ad51ab11f64480c579
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-30 17:49:53 -0600

    sketched out mysqluserdao methods (Phase 4)

---
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -0,0 +1,51 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MySqlUserDAO implements UserDAO {
+    public void MySqlAuthDAO() throws DataAccessException {
+        configure();
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAllUsers() {
+
+    }
+
+
+    private final String[] create = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+            `username` varchar(256) NOT NULL,
+            `password` VARCHAR(256) NOT NULL,
+            `email` VARCHAR(256) NOT NULL,
+            PRIMARY KEY (`username`)
+            )
+            """
+    };
+
+    private void configure() throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : create) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to configure database");
+        }
+    }
+}
