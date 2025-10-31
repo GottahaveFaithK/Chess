d36cc5b65b7cfaa9b7294247fbaa5b7a4b6dd314
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-30 20:50:46 -0600

    impl UserDAO tests (Phase 4)

--- server/src/test/java/dataaccess/UserDAOTest.java
+++ server/src/test/java/dataaccess/UserDAOTest.java
@@ -1,33 +1,68 @@
 package dataaccess;
 
-import org.junit.jupiter.api.BeforeEach;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserDAOTest {
     @Test
-    void createUser() {
-
+    void createUser() throws DataAccessException {
+        UserDAO userDAO = new MySqlUserDAO();
+        userDAO.deleteAllUsers();
+        UserData userData = new UserData("User", "Password", "myEmail@gmail.com");
+        userDAO.createUser(userData);
+        var res = userDAO.getUser("User");
+        assertNotNull(res);
+        assertEquals(userData.username(), res.username());
+        assertEquals(userData.password(), res.password());
+        assertEquals(userData.email(), res.email());
     }
 
     @Test
-    void duplicateUser() {
-
+    void duplicateUsername() throws DataAccessException {
+        UserDAO userDAO = new MySqlUserDAO();
+        userDAO.deleteAllUsers();
+        UserData userData = new UserData("User", "Password", "myEmail@gmail.com");
+        UserData secondUser = new UserData("User", "different person", "Other@gmail.com");
+        userDAO.createUser(userData);
+        DataAccessException ex = assertThrows(DataAccessException.class, () -> userDAO.createUser(secondUser));
+        assertTrue(ex.getMessage().startsWith("Username already taken"));
     }
 
     @Test
-    void getUser() {
-
+    void getUser() throws DataAccessException {
+        UserDAO userDAO = new MySqlUserDAO();
+        userDAO.deleteAllUsers();
+        UserData userData = new UserData("User", "Password", "myEmail@gmail.com");
+        userDAO.createUser(userData);
+        var res = userDAO.getUser("User");
+        assertNotNull(res);
+        assertEquals(userData.username(), res.username());
+        assertEquals(userData.password(), res.password());
+        assertEquals(userData.email(), res.email());
     }
 
     @Test
-    void getNonexistentUser() {
-
+    void getNonexistentUser() throws DataAccessException {
+        UserDAO userDAO = new MySqlUserDAO();
+        userDAO.deleteAllUsers();
+        DataAccessException ex = assertThrows(DataAccessException.class, () -> userDAO.getUser("User"));
+        assertEquals("User doesn't exist", ex.getMessage());
     }
 
     @Test
-    void deleteAllUsers() {
+    void deleteAllUsers() throws DataAccessException {
+        UserDAO userDAO = new MySqlUserDAO();
+        userDAO.deleteAllUsers();
+        UserData userData = new UserData("User", "Password", "myEmail@gmail.com");
 
+        userDAO.createUser(userData);
+        userDAO.deleteAllUsers();
+        DataAccessException ex = assertThrows(DataAccessException.class, () -> userDAO.getUser("User"));
+        assertEquals("User doesn't exist", ex.getMessage());
     }
 }
