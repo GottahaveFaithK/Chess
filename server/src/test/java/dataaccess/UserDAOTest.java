71d44cd416d5d67d38af7eecf1967d1c96ec89d6
Faith Kirkham <faithkirkham12@gmail.com>
2025-10-30 20:51:01 -0600

    impl UserDAO tests (Phase 4)

--- server/src/test/java/dataaccess/UserDAOTest.java
+++ server/src/test/java/dataaccess/UserDAOTest.java
@@ -1,10 +1,7 @@
 package dataaccess;
 
-import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.Test;
-
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
