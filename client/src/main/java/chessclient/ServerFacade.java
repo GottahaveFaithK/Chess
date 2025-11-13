07ef9b6def297e06463113992e24c5e2e4ea10ae
Faith Kirkham <faithkirkham12@gmail.com>
2025-11-12 22:13:25 -0700

    done for the night  (Phase 5)

--- client/src/main/java/chessclient/ServerFacade.java
+++ client/src/main/java/chessclient/ServerFacade.java
@@ -1,7 +1,25 @@
 package chessclient;
 
+import chess.ChessGame;
+
+import java.net.http.HttpRequest;
+import java.util.List;
+
 public class ServerFacade {
-    public ServerFacade(String serverUrl) {
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+        //TODO ask Bill about client
+    }
+
+    public List<ChessGame> listGames() {
+        return null;
+        //TODO ask Bill
+    }
+
+    private HttpRequest buildRequest() {
+        return null;
     }
     //figure out what this does
 }
