674a2259697b45d245841532c0f0452c6995dd85
Faith Kirkham <faithkirkham12@gmail.com>
2025-11-27 12:18:51 -0700

    added setters NotificationMessage ErrorMessage (phase 6)

--- shared/src/main/java/websocket/messages/NotificationMessage.java
+++ shared/src/main/java/websocket/messages/NotificationMessage.java
@@ -11,4 +11,8 @@ public class NotificationMessage extends ServerMessage {
     public String getMessage() {
         return message;
     }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
