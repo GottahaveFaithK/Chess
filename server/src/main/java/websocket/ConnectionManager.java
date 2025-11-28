30ee70624059264083caba72b86ae28f15dd774e
Faith Kirkham <faithkirkham12@gmail.com>
2025-11-28 12:14:00 -0700

    outline notes for shared websocket (phase 6)

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -1,4 +1,9 @@
 package websocket;
 
 public class ConnectionManager {
+    //look at petshop
+    //instead of just storing sessions, store a map with the game id as the key
+    //then the value for the game id would be a set of sessions that belong to that game
+
+    //broadcast idea is the same
 }
--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,4 +1,14 @@
 package websocket;
 
 public class WebSocketHandler {
+
+    //connect
+    //make move
+    //leave
+    //resign
+    //look at video at 11:02
+
+    //for deserializing make move commands may need to do it twice
+    //deserialize it to see what type it is
+    //if it is a make move command, deserialize again but as a make move command
 }
