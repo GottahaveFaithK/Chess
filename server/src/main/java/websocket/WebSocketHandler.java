700872bfa070e497ac0ff325a7dec90e2571ccb0
Faith Kirkham <faithkirkham12@gmail.com>
2025-11-28 18:02:01 -0700

    impl handleMessage? (phase 6)

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,9 +1,20 @@
 package websocket;
 
+import com.google.gson.Gson;
 import io.javalin.websocket.*;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.jetbrains.annotations.NotNull;
+import websocket.commands.UserGameCommand;
+
+
+import java.io.IOException;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final Gson gson = new Gson();
+
+
     @Override
     public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
 
@@ -11,14 +22,40 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     @Override
     public void handleConnect(WsConnectContext ctx) {
+        System.out.println("Websocket connected");
         ctx.enableAutomaticPings();
     }
 
     @Override
-    public void handleMessage(@NotNull WsMessageContext wsMessageContext) throws Exception {
+    public void handleMessage(WsMessageContext ctx) throws Exception {
+        String msg = ctx.message();
+        Session session = ctx.session;
 
+        UserGameCommand command = gson.fromJson(msg, UserGameCommand.class);
     }
 
+    @OnWebSocketMessage
+    public void onMessage(Session session, String msg) throws Exception {
+        UserGameCommand command = readJson(msg, GameCommand.class);
+
+        var conn = getConnection(command.authToken, session);
+        if (conn != null) {
+            switch (command.getcommandType()) {
+                case JOIN_PLAYER -> join(conn, msg);
+                case JOIN_OBSERVER -> observe(conn, msg);
+                case MAKE_MOVE -> move(conn, msg));
+                case LEAVE -> leave(conn, msg);
+                case RESIGN -> resign(conn, msg);
+            }
+        } else {
+            Connection.sendError(session.getRemote(), "unknown user");
+        }
+    }
+
+
+    private void enter() {
+
+    }
     //connect
     //make move
     //leave
