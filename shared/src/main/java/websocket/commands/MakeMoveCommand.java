5d2408f79be3a8b80da800afef685025f87b1039
Faith Kirkham <faithkirkham12@gmail.com>
2025-11-29 12:28:38 -0700

    added fields for easier messaging (phase 6)

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -4,13 +4,26 @@ import chess.ChessMove;
 
 public class MakeMoveCommand extends UserGameCommand {
     private final ChessMove move;
+    private final String startPos;
+    private final String endPos;
 
-    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move,
+                           String startPos, String endPos) {
         super(commandType, authToken, gameID);
         this.move = move;
+        this.startPos = startPos;
+        this.endPos = endPos;
     }
 
     public ChessMove getMove() {
         return move;
+    }
+
+    public String getStartPos() {
+        return startPos;
+    }
+
+    public String getEndPos() {
+        return endPos;
     }
 }
